f87c4aea7328573372cff41cd6e8c2b1c9324e71
Zheng_Da <74081090+SirLearning@users.noreply.github.com>
2024-04-01 14:37:11 +0800

    2024.4.1

--- 01PopAnalysis/src/SubsampleReads.java
+++ 01PopAnalysis/src/SubsampleReads.java
@@ -1,16 +1,22 @@
-
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SubsampleReads {
-    public void Subsample(String infile, String outfile, String bases) {
-        this.Subsample("1", infile, outfile, bases, "11000");
+    public static void main(String[] args) {
+        SubsampleReads subsampleReads = new SubsampleReads();
+        subsampleReads.Subsample(args[0], args[1], args[2], args[3]);
     }
-    public void Subsample(String seed, String infile, String outfile, String bases, String averageLength) {
+    private static final Logger logger = LoggerFactory.getLogger(SubsampleReads.class);
+    @SuppressWarnings("unused")
+    public void Subsample(String infile, String outfile, String readsNum) {
+        this.Subsample("1", infile, outfile, readsNum);
+    }
+    public void Subsample(String seed, String infile, String outfile, String readsNum) {
         long timeAll = System.currentTimeMillis();
         try {
             BufferedReader br = new BufferedReader(new FileReader(infile));
@@ -23,12 +29,14 @@ public class SubsampleReads {
             long timeRead = System.currentTimeMillis();
             String ID = br.readLine().substring(1);
             while ((temp = br.readLine()) != null) {
-                if (temp.startsWith(">")) {
+                if (temp.startsWith("@")) {
                     IDseq.put(ID, sb.toString());
                     ID = temp.substring(1);
                     sb.delete(0, sb.length());
+                    br.readLine();
+                    br.readLine();
                 } else {
-                    sb.append(temp);
+                    sb.append(temp.substring(1));
                 }
             }
             IDseq.put(ID, sb.toString());
@@ -37,36 +45,31 @@ public class SubsampleReads {
 
             // shuffle
             long timeShuffle = System.currentTimeMillis();
-            List list_shuffle = new ArrayList<>();
-            list_shuffle.addAll(IDseq.keySet());
+            List<String> list_shuffle = new ArrayList<>(IDseq.keySet());
             Random rnd = new Random(Integer.parseInt(seed));
             Collections.shuffle(list_shuffle, rnd);
-
             // sample of reads
-            int readsNum = Math.round(Float.parseFloat(bases)/Float.parseFloat(averageLength));
-            sampleSet.addAll(list_shuffle.subList(0, readsNum));
+            int reads = Integer.parseInt(readsNum);
+            sampleSet.addAll(list_shuffle.subList(0, reads));
             System.out.println("Shuffle and hashset time: " + (System.currentTimeMillis() - timeShuffle));
 
             long timeWrite = System.currentTimeMillis();
-            long sumBases = 0;
             long readCount = 0;
             for (String s : sampleSet) {
                 bw.write(">" + s + "\n");
                 bw.write(IDseq.get(s) + "\n");
                 readCount++;
-                sumBases += IDseq.get(s).length();
-                if (sumBases > Long.parseLong(bases)) break;
             }
 
             bw.flush();
             bw.close();
             System.out.println("final time: " + (System.currentTimeMillis() - timeWrite));
-            System.out.println("final bases count: " + sumBases);
             System.out.println("final reads count: " + readCount);
 
         } catch (Exception e){
-            e.printStackTrace();
+            logger.error("An error occurred while subsampling reads", e);
         }
         System.out.println("all time: " + (System.currentTimeMillis() - timeAll));
     }
 }
+
